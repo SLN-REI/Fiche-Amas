db04976da5e0f62a3a1c70fe3ddec081ff55367a
SLN-REI <120612765+SLN-REI@users.noreply.github.com>
2024-02-20 14:49:08 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,25 +41,7 @@ if st.sidebar.button("voir la fiche"):
     st.write(f"Contenu de l'onglet 'toto' pour la date {selected_date}")
     
     container=st.container()
-    container.write("Le gisement est situé au cœur du Plateau de Thio sur la rive gauche de la rivière Thio. Il est cerné par les grandes structures traversant ce plateau : la N50 de Sans Culotte et la N140 de Clémence ainsi qu'une structure N140 au sud-ouest.  La topographie d'origine était celle d'une dépression de grande taille entouré par des mamelons de roches dures ou saines, sous les murs des failles majeures.  A l'ouest il y une zone bréchique siliceuse et la minéralisation y est erratique. L'altération est majoritairement de type altéré dur, issu des harzburgites de faciès normal 
-(FICHE_RER _AMAS_2018_MT_PLT_GRAND_SAINT_PIERRE)
-
-
-L’altitude moyenne du Plateau est comprise entre 500 et 550 m. Ce plateau comporte des petits dômes de péridotites, vestiges d’une surface plus élevée aujourd’hui démantelée. Les sommets de ces dômes peuvent atteindre 650 m.
-- Le gisement de BELVEDERE OUEST se situe à la pointe Sud Est du plateau de Thio.
-- L'amas de BELVEDERE EST se situe à la pointe Sud Est du plateau de Thio.
-- Le gisement CLEMENCE est situé au Nord du Plateau de Thio.
-- L'amas DUC DE WELLINGTON se situe à environ 600m à l'est de l'amas CLEMENCE.
-- L’amas HAPPY GO LUCKY est situé dans le domaine de Thio Plateau, à environ 1.5 km à vol d’oiseau, au Nord-Ouest du « Tritout ». Cet amas marque l’extrémité Nord-Ouest du Plateau de Thio. Happy Go Lucky est constitué de trois lignes de crêtes de direction NW-SE, reliées entre elles vers le Sud où elles forment un vaste plateau latéritique. L’altitude varie entre 300 et 600 m.
-- L'amas SANTA MARIA constitue la portion Ouest de la cuvette de la fosse SAINT PIERRE située sur le plateau de Thio. - L'amas LOUISE est dans la continuité Nord de Santa Maria.
-- Le gisement de GRAND SAINT PIERRE est situé au cœur du Plateau de Thio sur la rive gauche de la rivière Thio.
-(Rapport_declaration2020_Ressources&Reserves_JORC_THIO_210331_PLAN5&10ANS_final)
-
-
-Le plateau de Thio est constitué d’un ensemble de péridotites constitué de harzburgites et de dunites partiellement serpentinisées où dominent principalement les harzburgites. Au sein de cet ensemble, s’intercalent parfois des niveaux centimétriques de pyroxénolites et des niveaux dunitiques qui eux, peuvent atteindre des épaisseurs de plusieurs dizaines de mètres. Ces différents éléments définissent le rubanement. Les directions et pendages moyens mesurés sur le rubanement sont de 125°E à pendage 40°N sur l’ensemble du plateau, avec une intensité variable et croissante du NE vers le SW.
-Ce massif repose sur un substratum volcanique basaltique (Paléocène – Eocène) selon un contact anormal souligné par une silicification intense de la semelle constituée de serpentinites mylonitisées. 
-(Ra-11-0497_C_PLT_PE_V2)
-")
+    container.write("aaa")
   with tabs[1]:
       st.write(f"Contenu de l'onglet 'tata' pour la date {selected_date}")
     
