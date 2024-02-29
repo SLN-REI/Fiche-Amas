6546376b003ff204c01156b8d27088e1c05ed4b6
SLN-REI <120612765+SLN-REI@users.noreply.github.com>
2024-03-01 09:27:09 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,37 +16,28 @@ def spacer(n, line=True) :
 st.sidebar.write("Choix de l'année de la fiche :")
 selected_date  = st.sidebar.selectbox("Date de MAJ de la fiche :", ["2023", "2024"], index=1)
 st.sidebar.write("Choix du site :")
-selected_site  = st.sidebar.selectbox("selection du site :", ["Thio Plateau","Thio Camps des sapins", "Népoui", "Kouaoua"], index=1)
+selected_site  = st.sidebar.selectbox("Sélection du site :", ["Thio Plateau","Thio Camps des sapins", "Népoui", "Kouaoua"], index=1)
 if selected_site == "Thio Plateau":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["belvedere", "clemence", "gsp", "santamaria"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["belvedere", "clemence", "gsp", "santamaria"], index=1)
 elif selected_site == "Thio Camps des sapins":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["3piments", "pt171"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["3piments", "pt171"], index=1)
 elif selected_site == "Népoui":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["ballantine", "bernablanche", "krans", "montvert", "paidi", "surprise4"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["ballantine", "bernablanche", "krans", "montvert", "paidi", "surprise4"], index=1)
 elif selected_site == "Kouaoua":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["ednkarembe", "extpentecost", "marmelade", "nordmea", "sousbureaux", "stamboul"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["ednkarembe", "extpentecost", "marmelade", "nordmea", "sousbureaux", "stamboul"], index=1)
 elif selected_site == "Poro":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["bonini", "francaise", "mecoyamatos"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["bonini", "francaise", "mecoyamatos"], index=1)
 elif selected_site == "Thiebagui":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["alpha", "dome"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["alpha", "dome"], index=1)
 elif selected_site == "Tontouta":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["colthomp", "fernandepaul"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["colthomp", "fernandepaul"], index=1)
 elif selected_site == "Dothio":
-  selected_amas  = st.sidebar.selectbox("selection de l'amas :", ["pauline", "revanche"], index=1)
+  selected_amas  = st.sidebar.selectbox("Sélection de l'amas :", ["pauline", "revanche"], index=1)
 
 # file_path_text_generalite = os.path.join("Fiches Amas", selected_site, selected_amas, "generalite.txt")
 
-# # Vérifier si le fichier existe
-# if os.path.exists(file_path):
-#     # Lire le contenu du fichier texte
-#     with open(file_path, 'r') as file:
-#         text_content = file.read()
-
-#     # Afficher le contenu du fichier texte
-#     st.text("Contenu du fichier texte :")
-#     st.text(text_content)
-# else:
-#     st.error("Le fichier 'tata.txt' n'a pas été trouvé dans le dossier 'toto'.")
+image_path = "C:\Users\octave.reignier\OneDrive - Eramet SA\Documents\0 - Py\Fiches Amas\THI_PLT_GSP\1_GENERALITES\DIVERS\2023\cadastre_Plateau.png"
+image = st.image(image_path, caption="Description de l'image", use_column_width=True)
 
 if st.sidebar.button("voir la fiche"):
   st.write("Vous consultez le site de",selected_site,"et l'amas",selected_amas, "sur l'année",selected_date)  
